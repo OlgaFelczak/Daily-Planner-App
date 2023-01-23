ab53d4e0f123a99cd9d1fe0264afb74e67e238fe
Olga Felczak <ofelczak@gmail.com>
2023-01-23 16:54:19 +0000

    Added render Timeblock function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,52 @@
-// Current day ID element
 let currentDateJqEl = $("#currentDay");
 let currentDate = new Date();
 
+// Get the current day and display it at the top of the calendar
 currentDateJqEl.append(moment(currentDate).format("dddd, MMMM, Do"));
 
 // Timeblocks element container
 const containerJqEl = $(".container");
 const timeblockJqEl = $("<div>").attr("class", "time-block");
-containerEl.append(timeblockJqEl);
+containerJqEl.append(timeblockJqEl);
 
-// Get the current day and display it at the top of the calendar
+function renderTimeblock() {
+  const rowJqEl = $("<div>").attr("class", "row");
+  const hourJqEl = $("<div>").attr("class", "hour");
+  const textareaJqEl = $("<textarea>").attr("class", "past");
+  const buttonJqEl = $("<button>").attr("class", "saveBtn");
+
+  rowJqEl.append(hourJqEl);
+  rowJqEl.append(textareaJqEl);
+  rowJqEl.append(buttonJqEl);
+  timeblockJqEl.append(rowJqEl);
+}
+renderTimeblock();
+
+{
+  /* <div class="time-block">
+  <div class="row">
+    <div class="hour">09</div>
+    <textarea class="past"></textarea>
+    <button class="saveBtn">
+      <i class="fa-solid fa-floppy-disk"></i>
+    </button>
+  </div>
+  <div class="row">
+    <div class="hour">10</div>
+    <textarea class="present"></textarea>
+    <button class="saveBtn">
+      <i class="fa-solid fa-floppy-disk"></i>
+    </button>
+  </div>
+  <div class="row">
+    <div class="hour">11</div>
+    <textarea class="future">sdfuasiudyfiu</textarea>
+    <button class="saveBtn">
+      <i class="fa-solid fa-floppy-disk"></i>
+    </button>
+  </div>
+</div>; */
+}
 
 // Present timeblocks for standard business hours when the user scrolls down
 
