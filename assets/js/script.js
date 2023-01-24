424b81381edb2cd8619c7d474b54425fca0d95a8
Olga Felczak <ofelczak@gmail.com>
2023-01-24 12:41:47 +0000

    Updated function renderTimeblock to renderRow with conditions to view, retrieve data from localstorage, added save button, and persist data between refreshes of a page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-let currentDateJqEl = $("#currentDay");
-let currentDate = new Date();
+const workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Get the current day and display it at the top of the calendar
+let currentDateJqEl = $("#currentDay");
+let currentDate = new Date();
 currentDateJqEl.append(moment(currentDate).format("dddd, MMMM, Do"));
 
 // Timeblocks element container
@@ -9,11 +10,19 @@ const containerJqEl = $(".container");
 const timeblockJqEl = $("<div>").attr("class", "time-block");
 containerJqEl.append(timeblockJqEl);
 
+// Present timeblocks for standard business hours when the user scrolls down
+for (let i = 0; i < workingHours.length; i++) {
+  const hour = workingHours[i];
+  renderRow(hour);
+}
+
 function renderRow(hour) {
   const rowJqEl = $("<div>").attr("class", "row");
 
+  // Textarea to allow a user to enter an event when they click a timeblock
   const textareaJqEl = $("<textarea>");
   const currentHour = Number(moment(new Date()).format("H"));
+  // Color-code each timeblock based on past, present, and future when the timeblock is viewed
   if (hour < currentHour) {
     textareaJqEl.attr("class", "past");
   } else if (hour > currentHour) {
@@ -21,11 +30,17 @@ function renderRow(hour) {
   } else {
     textareaJqEl.attr("class", "present");
   }
+  // Retrieve events data from localstorage
+  textareaJqEl.text(JSON.parse(localStorage.getItem(hour)));
 
   const hourJqEl = $("<div>").attr("class", "hour").text(hour);
-  const buttonJqEl = $("<button>").attr("class", "saveBtn");
-  const buttonIconJqEl = $("<i>").attr("class", "fa-solid fa-floppy-disk");
 
+  const buttonJqEl = $("<button>").attr("class", "saveBtn");
+  // Persist events data between refreshes of a page
+  buttonJqEl.on("click", function () {
+    localStorage.setItem(hour, JSON.stringify(textareaJqEl.val()));
+  });
+  const buttonIconJqEl = $("<i>").attr("class", "fa-solid fa-floppy-disk");
 
   buttonJqEl.append(buttonIconJqEl);
   rowJqEl.append(hourJqEl);
@@ -33,42 +48,3 @@ function renderRow(hour) {
   rowJqEl.append(buttonJqEl);
   timeblockJqEl.append(rowJqEl);
 }
-renderRow(9);
-renderRow(18);
-renderRow(20);
-
-{
-  /* <div class="time-block">
-  <div class="row">
-    <div class="hour">09</div>
-    <textarea class="past"></textarea>
-    <button class="saveBtn">
-      <i class="fa-solid fa-floppy-disk"></i>
-    </button>
-  </div>
-  <div class="row">
-    <div class="hour">10</div>
-    <textarea class="present"></textarea>
-    <button class="saveBtn">
-      <i class="fa-solid fa-floppy-disk"></i>
-    </button>
-  </div>
-  <div class="row">
-    <div class="hour">11</div>
-    <textarea class="future">sdfuasiudyfiu</textarea>
-    <button class="saveBtn">
-      <i class="fa-solid fa-floppy-disk"></i>
-    </button>
-  </div>
-</div>; */
-}
-
-// Present timeblocks for standard business hours when the user scrolls down
-
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
-// Allow a user to enter an event when they click a timeblock.
-
-// Save the event in local storage when the save button is clicked in that timeblock.
-
-// Persist events between refreshes of a page.
